43f3bdd163b6a74bf096bad01c2ffb2884361c7b
dorgee <dorgee.lama@uleth.ca>
2025-10-17 21:38:05 -0600

    display Menu Done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+
+
+void displayMenu(){
+  std::cout << "Enter your choice:" << "\n";
+  std::cout << "1) Add new patient" << "\n";
+  std::cout << "2) Print all patient" << "\n";
+  std::cout << "3) Get next patients" << "\n";
+  std::cout << "4) Exit" << "\n";
+}
+
+
+
 int main() {
+
+  while(1){
+  displayMenu();
+  int choice;
+  std::cin >> choice;
+  }
 
   return 0;
 }
