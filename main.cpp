a88d770128bbb16ece4ea2b2449554921edb4200
dorgee <dorgee.lama@uleth.ca>
2025-10-19 16:37:52 -0600

    intial add patient logic added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
 
+const int specializations = 20;
+const int queueSize = 5;
+
+int hospitalQueue[specializations][queueSize]{3};
+std::string namesOfPatient[specializations][queueSize]{""};
 
 void displayMenu(){
   std::cout << "Enter your choice:" << "\n";
@@ -9,8 +15,41 @@ void displayMenu(){
   std::cout << "4) Exit" << "\n";
 }
 
+bool checkIfQueueFull(int specialization){
+  int targetSpecialization[queueSize];
+  for(int i = 0; i < queueSize; i++){
+    targetSpecialization[i] = hospitalQueue[specialization][i];
+  }
+
+  for(int patient : targetSpecialization){
+    std::cout << patient;
+    if(patient == 3){
+      return true;
+    }
+  }
+  return false;
+
+}
+
 void addPatient(){
-  std::cout << "adding patient" <<"\n"; 
+  std::cout << "Enter Specialization, name, status: " <<"\n"; 
+  int specialization{};
+  int status{};
+  std::string name{};
+  std::cin >> specialization >> name >> status;
+  std::cout << specialization << " " << name << " " << status << "\n";
+  if(checkIfQueueFull(specialization)){
+    std::cout << "Sorry the queue is full right now. Not accepting new patients" << "\n";
+    return;
+  }
+  if(status == 1){
+    //shift all elements right
+    hospitalQueue[specialization][0] = 1;
+  }
+  else{
+    // shift all elements left
+  }
+
 
 }
 
@@ -31,7 +70,7 @@ int main() {
   bool exitProgram = true;
   while(exitProgram){
   displayMenu();
-  int choice;
+  int choice{};
   std::cin >> choice;
 
   switch(choice){
