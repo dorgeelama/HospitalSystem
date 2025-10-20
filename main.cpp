e7234899fc38af29c4df7c771b12bd8ea7c2a359
Dorgee WSL <dorgeelama91@gmail.com>
2025-10-19 22:22:59 -0600

    add patient logic finished

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 const int specializations = 20;
 const int queueSize = 5;
 
-int hospitalQueue[specializations][queueSize]{3};
-std::string namesOfPatient[specializations][queueSize]{""};
+int hospitalQueue[specializations][queueSize]{};
+std::string namesOfPatient[specializations][queueSize];
 
 void displayMenu(){
   std::cout << "Enter your choice:" << "\n";
@@ -13,6 +13,7 @@ void displayMenu(){
   std::cout << "2) Print all patient" << "\n";
   std::cout << "3) Get next patients" << "\n";
   std::cout << "4) Exit" << "\n";
+  std::cout << "5) Print current queue" << "\n";
 }
 
 bool checkIfQueueFull(int specialization){
@@ -22,13 +23,19 @@ bool checkIfQueueFull(int specialization){
   }
 
   for(int patient : targetSpecialization){
-    std::cout << patient;
     if(patient == 3){
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
+}
 
+void initializeHospitalSystem() {
+  for(int i = 0; i < specializations; i++){
+    for(int j = 0; j < queueSize; j++){
+      hospitalQueue[i][j] = 3;
+    }
+  }
 }
 
 void addPatient(){
@@ -38,17 +45,40 @@ void addPatient(){
   std::string name{};
   std::cin >> specialization >> name >> status;
   std::cout << specialization << " " << name << " " << status << "\n";
+  int start = 0;
+  int end = queueSize - 1;
   if(checkIfQueueFull(specialization)){
     std::cout << "Sorry the queue is full right now. Not accepting new patients" << "\n";
     return;
   }
   if(status == 1){
-    //shift all elements right
-    hospitalQueue[specialization][0] = 1;
+    while(hospitalQueue[specialization][start] != 3){
+      start++;
+    }
+    if(start > 0){
+     for(int i = start; i > 0; i--){
+        hospitalQueue[specialization][i] = hospitalQueue[specialization][i - 1];
+        namesOfPatient[specialization][i] = namesOfPatient[specialization][i -1];
+      }
+    }
+    hospitalQueue[specialization][0] = status;
+    namesOfPatient[specialization][0] = name;
   }
   else{
-    // shift all elements left
+    while(hospitalQueue[specialization][end] != 3){
+      end--;
+    }
+    if(end < queueSize - 1){
+      for (int i = end; i < queueSize - 1; i++){
+        hospitalQueue[specialization][i] = hospitalQueue[specialization][i + 1];
+        namesOfPatient[specialization][i] = namesOfPatient[specialization][i + 1];
+        }
+    }
+    hospitalQueue[specialization][queueSize -  1] = status;
+    namesOfPatient[specialization][queueSize - 1] = name;
   }
+  start = 0;
+  end = queueSize - 1;
 
 
 }
@@ -63,11 +93,21 @@ void getNextPatient(){
 
 }
 
+// This function is only for debugging purposes 
+void printCurrentQueue() {
+  for(int i = 0; i < 5; i++){
+    std::cout << hospitalQueue[0][i] << " ";
+    std::cout << namesOfPatient[0][i] << " " << "\n";
+  }
+  std::cout << "\n";
+}
+
 
 
 int main() {
 
   bool exitProgram = true;
+  initializeHospitalSystem();
   while(exitProgram){
   displayMenu();
   int choice{};
@@ -82,6 +122,9 @@ int main() {
       break;
     case 3:
       getNextPatient();
+      break;
+    case 5:
+      printCurrentQueue();
       break;
     default:
       exitProgram = false;
